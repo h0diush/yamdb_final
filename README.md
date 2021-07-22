b102f7fc8c40e6fbada06ab0cff10d583bb74eb1
h0diush <>
2021-07-23 01:15:43 +0300

    final

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 yamdb_final
 
 
-![example workflow](https://github.com/h0diush/yamdb_final/actions/workflows/yamdb_workflow/badge.svg)
+https://github.com/h0diush/yamdb_final/actions/workflows/Django-app%20workflow/badge.svg
--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -18,7 +18,12 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '130.193.42.124', 
+    'localhost', 
+    'finalyamdb.tk',
+    'www.finalyamdb.tk'
+] 
 
 
 # Application definition
