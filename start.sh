c4043dd8c4dabecf32a13b66ba878135535e928c
h0diush <>
2021-07-23 16:57:02 +0300

    final

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ python3 manage.py migrate --noinput
 
 python3 manage.py collectstatic --noinput
 
-gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000
+gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000 -reload -w 4
