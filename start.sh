102d9271617ce0e5d3c4f79988a378cb3ba741fc
h0diush <>
2021-07-23 16:48:38 +0300

    final

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
-python manage.py makemigrations categories
-python manage.py makemigrations reviews
-python manage.py makemigrations users
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+python3 manage.py makemigrations
+
+python3 manage.py migrate --noinput
+
+python3 manage.py collectstatic --noinput
+
 gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000
