c90996040136641a90d6b6683be8fb6ca7871949
h0diush <>
2021-07-23 17:10:28 +0300

    f1nal

--- start.sh
+++
@@ -1,7 +0,0 @@
-python3 manage.py makemigrations
-
-python3 manage.py migrate --noinput
-
-python3 manage.py collectstatic --noinput
-
-gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000 -reload -w 4
